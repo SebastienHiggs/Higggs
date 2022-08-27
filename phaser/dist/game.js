d6cbdf678502a611745c9cd9182ee9c240346a83
Sebastien Higgs <sebastienjhiggs@gmail.com>
2022-08-27 13:23:15 +1000

    background

--- phaser/dist/game.js
+++ phaser/dist/game.js
@@ -1,6 +1,11 @@
 //scene
 let gameScene = new Phaser.Scene('Game');
 
+//load assets
+gameScene.preload = function(){
+    this.load.image('background','phaser/imgs/back.gif');
+}
+
 //config
 let config = {
     type: Phaser.AUTO,
