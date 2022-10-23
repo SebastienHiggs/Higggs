f7f6ac2e0e1bce3c81f980c58260a0f1007147b9
Sebastien Higgs <sebastienjhiggs@gmail.com>
2022-10-23 17:42:58 +1100

    first css and js

--- phaser/dist/game.js
+++ phaser/dist/game.js
@@ -1,5 +1,6 @@
 //scene
 let gameScene = new Phaser.Scene('Game');
+windOn = 1;
 
 //load assets
 gameScene.preload = function(){
@@ -8,9 +9,10 @@ gameScene.preload = function(){
     this.load.image('solar','phaser/imgs/solar.png');
     this.load.image('wind','phaser/imgs/wind.png');
     this.load.image('wind2','phaser/imgs/wind2.png');
-    windImg = 1;
+    
 
-    this.load.atlas('winda', 'phaser/imgs/winda.png', 'phaser/imgs/winda.json');
+    //this.load.atlas('winda', 'phaser/imgs/winda.png', 'phaser/imgs/winda.json');
+    this.load.spritesheet('winds', 'phaser/imgs/winda.png', { frameWidth: 1493 , frameHeight: 1299, endFrame: 2 });
 
     this.load.audio('cli','phaser/imgs/cli.mp3');
     this.load.audio('ck','phaser/imgs/ck.mp3');
@@ -20,63 +22,65 @@ gameScene.preload = function(){
 gameScene.create = function() {
     let bg = this.add.sprite(0,0,'background');
     bg.setOrigin(0,0);
-    //let pp = this.add.sprite(1700,500,'powerplant');
-    //pp.depth =  1; //not necessary here but good for refence
-    //pp.setScale(0.3);
 
     let cliSound = this.sound.add('cli');
     let ckSound = this.sound.add('ck');
 
-    this.anims.create({
-        key: 'wind',
-        frames: 'winda',
-        frameRate: 12,
-        repeat: -1
-    });
 
-    this.add.sprite(400, 300).play('wind');
-
-    this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNumbers('brawler', { frames: [ 0, 1, 2, 3 ] }),
-        frameRate: 8,
+    const windConfig = {
+        key: 'wind1',
+        frames: this.anims.generateFrameNumbers('winds', { frames: [ 0, 1 ] }),
+        frameRate: 4,
         repeat: -1
-    });
+    };
+
+    const noWindConfig = {
+        key: 'wind2',
+        frames: this.anims.generateFrameNumbers('winds', { frames: [ 0, 0] }),
+        frameRate: 10,
+        repeat: -1
+    };
+
+    const current = this.add.text(48, 460, 'Playing: walk', { color: '#00ff00' });
+
+    this.anims.create(windConfig);
+    var windy = this.add.sprite(1350, 450, 'winds').setInteractive();
+
+    windy.setScale(0.13);
+    if (windOn == 0) {
+        windy.play('wind2');
+        current.setText('wind off');
+    } else {
+        windy.play('wind1');
+        current.setText('wind on');
+    }
     
     var solar = this.add.sprite(1200, 505, 'solar');
     solar.setScale(0.05);
     var solar2 = this.add.sprite(1130, 510, 'solar');
     solar2.setScale(0.05);
 
-    var wind = this.add.sprite(1350, 450, 'wind').setInteractive();
-    wind.setScale(0.2);
-
-    wind.on('pointerdown', function (pointer) {
+    windy.on('pointerdown', function (pointer) {
         cliSound.play();
-        wind.setScale(0.22);
+        windy.setScale(0.14);
         bigW = true;
-        if (windImg == 2) {
-            wind.setTexture('wind2');
-            windImg = 1;
-        } else {
-            wind.setTexture('wind');
-            windImg = 2;
-        }
-        
+        windOn = 0;
     });
-    wind.on('pointerout', function (pointer) {
-        wind.setScale(0.2);
+    windy.on('pointerout', function (pointer) {
+        windy.setScale(0.13);
         if (bigW == true) {
             ckSound.play();
         };
         bigW = false;
+        windOn = 0;
     });
-    wind.on('pointerup', function (pointer) {
-        wind.setScale(0.2);
+    windy.on('pointerup', function (pointer) {
+        windy.setScale(0.13);
         if (bigW == true) {
             ckSound.play();
         };
         bigW = false;
+        windOn = 0;
     });
 
     var pp = this.add.sprite(1700, 500, 'powerplant').setInteractive();
