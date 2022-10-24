e9fa0912bd1bb7b7d4e39b19baca346cc773d2d9
Sebastien Higgs <sebastienjhiggs@gmail.com>
2022-10-24 23:13:41 +1100

    started scaling and general fixes

--- phaser/dist/game.js
+++ phaser/dist/game.js
@@ -1,17 +1,29 @@
+//Global Variables
+width = window.innerWidth;
+height = window.innerHeight;
+yup = 1;
+
+if (width > height*1.9) {
+    sceneHeight = Math.round(height*0.9);
+    sceneWidth = Math.round(sceneHeight*2);
+} else {
+    sceneWidth = Math.round(width*0.95);
+    sceneHeight = Math.round(sceneWidth/2);
+}
+
+var basicScale = sceneWidth/1816;
+
 //scene
 let gameScene = new Phaser.Scene('Game');
-windOn = 1;
 
 //load assets
-gameScene.preload = function(){
+gameScene.preload = function() {
     this.load.image('background','phaser/imgs/back.png');
     this.load.image('powerplant','phaser/imgs/powerplant.png');
     this.load.image('solar','phaser/imgs/solar.png');
     this.load.image('wind','phaser/imgs/wind.png');
     this.load.image('wind2','phaser/imgs/wind2.png');
     
-
-    //this.load.atlas('winda', 'phaser/imgs/winda.png', 'phaser/imgs/winda.json');
     this.load.spritesheet('winds', 'phaser/imgs/winda.png', { frameWidth: 1493 , frameHeight: 1299, endFrame: 2 });
 
     this.load.audio('cli','phaser/imgs/cli.mp3');
@@ -20,12 +32,15 @@ gameScene.preload = function(){
 
 //called once after preload
 gameScene.create = function() {
+    windOn = 1;
+
     let bg = this.add.sprite(0,0,'background');
     bg.setOrigin(0,0);
 
     let cliSound = this.sound.add('cli');
     let ckSound = this.sound.add('ck');
 
+    bg.setScale(sceneWidth/1816); //back.png is 1816*908
 
     const windConfig = {
         key: 'wind1',
@@ -43,24 +58,29 @@ gameScene.create = function() {
 
     const current = this.add.text(48, 460, 'Playing: walk', { color: '#00ff00' });
 
+    //Creating the Windmills
     this.anims.create(windConfig);
     var windy = this.add.sprite(1350, 450, 'winds').setInteractive();
 
     windy.setScale(0.13);
     if (windOn == 0) {
         windy.play('wind2');
-        current.setText('wind off');
+        current.setText("wind off");
     } else {
         windy.play('wind1');
-        current.setText('wind on');
+        current.setText((sceneWidth));//+toString(sceneHeight));
     }
     
-    var solar = this.add.sprite(1200, 505, 'solar');
-    solar.setScale(0.05);
-    var solar2 = this.add.sprite(1130, 510, 'solar');
-    solar2.setScale(0.05);
+    //Creating the Solar Panels
+    var solar = this.add.sprite(sceneWidth*0.62, sceneHeight*0.57, 'solar');
+    solar.setScale(0.05*basicScale);
+    var solar2 = this.add.sprite(sceneWidth*0.66, sceneHeight*0.56, 'solar');
+    solar2.setScale(0.05*basicScale);
 
-    windy.on('pointerdown', function (pointer) {
+    clickScale(solar,0.05);
+
+    //Move the windmills?
+    /*windy.on('pointerdown', function (pointer) {
         cliSound.play();
         windy.setScale(0.14);
         bigW = true;
@@ -81,24 +101,25 @@ gameScene.create = function() {
         };
         bigW = false;
         windOn = 0;
-    });
+    });*/
 
-    var pp = this.add.sprite(1700, 500, 'powerplant').setInteractive();
-    pp.setScale(0.3);
+    //Make the powerplant
+    var pp = this.add.sprite(sceneWidth*0.9, sceneHeight*0.56, 'powerplant').setInteractive();
+    pp.setScale(0.3*basicScale);
     pp.on('pointerdown', function (pointer) {
         cliSound.play();
-        pp.setScale(0.32);
+        pp.setScale(0.32*basicScale);
         bigP = true;
     });
     pp.on('pointerout', function (pointer) {
-        pp.setScale(0.3);
+        pp.setScale(0.3*basicScale);
         if (bigP == true) {
             ckSound.play();
         };
         bigP = false;
     });
     pp.on('pointerup', function (pointer) {
-        pp.setScale(0.3);
+        pp.setScale(0.3*basicScale);
         if (bigP == true) {
             ckSound.play();
         };
@@ -106,11 +127,35 @@ gameScene.create = function() {
     });
 };
 
+function clickScale(spriteName,itemScale) {
+    spriteName.setScale(itemScale*basicScale);
+    spriteName.on('pointerdown', function (pointer) {
+        cliSound.play();
+        spriteName.setScale(1.07*itemScale*basicScale);
+        bigP = true;
+    });
+    spriteName.on('pointerout', function (pointer) {
+        pp.setScale(itemScale*basicScale);
+        if (bigP == true) {
+            ckSound.play();
+        };
+        bigP = false;
+    });
+    spriteName.on('pointerup', function (pointer) {
+        spriteName.setScale(itemScale*basicScale);
+        if (bigP == true) {
+            ckSound.play();
+        };
+        bigP = false;
+    });
+}
+
+
 //config
-let config = {
+var config = {
     type: Phaser.AUTO,
-    width: 1875,
-    height: 880,
+    width: sceneWidth,
+    height: sceneHeight,
     scene: gameScene
 };
 
