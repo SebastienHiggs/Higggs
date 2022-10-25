14795e39a314f89fb2472bfa3520907456fb88d3
Sebastien Higgs <sebastienjhiggs@gmail.com>
2022-10-25 21:39:44 +1100

    solar is interactive, night works - all producers should be set up

--- phaser/dist/game.js
+++ phaser/dist/game.js
@@ -2,6 +2,8 @@
 width = window.innerWidth;
 height = window.innerHeight;
 yup = 1;
+var windOn = 0;
+var night = 0;
 
 if (width > height*1.9) {
     sceneHeight = Math.round(height*0.9);
@@ -19,6 +21,8 @@ let gameScene = new Phaser.Scene('Game');
 //load assets
 gameScene.preload = function() {
     this.load.image('background','phaser/imgs/back.png');
+    this.load.image('night','phaser/imgs/night.png');
+    this.load.image('sun','phaser/imgs/sun.png');
     this.load.image('powerplant','phaser/imgs/powerplant.png');
     this.load.image('solar','phaser/imgs/solar.png');
     this.load.image('wind','phaser/imgs/wind.png');
@@ -32,10 +36,14 @@ gameScene.preload = function() {
 
 //called once after preload
 gameScene.create = function() {
-    windOn = 1;
+    
 
     let bg = this.add.sprite(0,0,'background');
     bg.setOrigin(0,0);
+
+    let nightbg = this.add.sprite(0,0,'night');
+    nightbg.setOrigin(0,0);
+    nightbg.setScale(0.001)
 
     let cliSound = this.sound.add('cli');
     let ckSound = this.sound.add('ck');
@@ -49,113 +57,153 @@ gameScene.create = function() {
         repeat: -1
     };
 
-    const noWindConfig = {
-        key: 'wind2',
-        frames: this.anims.generateFrameNumbers('winds', { frames: [ 0, 0] }),
-        frameRate: 10,
-        repeat: -1
-    };
-
-    const current = this.add.text(48, 460, 'Playing: walk', { color: '#00ff00' });
-
     //Creating the Windmills
     this.anims.create(windConfig);
-    var windy = this.add.sprite(1350, 450, 'winds').setInteractive();
+    var windy = this.add.sprite(sceneWidth*0.74, sceneHeight*0.47, 'winds').setInteractive();
 
-    windy.setScale(0.13);
-    if (windOn == 0) {
-        windy.play('wind2');
-        current.setText("wind off");
-    } else {
-        windy.play('wind1');
-        current.setText((sceneWidth));//+toString(sceneHeight));
-    }
-    
-    //Creating the Solar Panels
-    var solar = this.add.sprite(sceneWidth*0.62, sceneHeight*0.57, 'solar');
-    solar.setScale(0.05*basicScale);
-    var solar2 = this.add.sprite(sceneWidth*0.66, sceneHeight*0.56, 'solar');
-    solar2.setScale(0.05*basicScale);
+    windy.setScale(0.13*basicScale);
 
-    clickScale(solar,0.05);
-
-    //Move the windmills?
-    /*windy.on('pointerdown', function (pointer) {
+    windy.on('pointerdown', function (pointer) {
         cliSound.play();
-        windy.setScale(0.14);
-        bigW = true;
-        windOn = 0;
+        windy.setScale(0.14*basicScale);
+        mouseOver = true;
+        if (windOn == 0) {
+            windy.play({
+                key: 'wind1',
+                repeat: -1
+            });
+            windOn = 1;
+        } else {
+            windy.play({
+                key: 'wind1',
+                repeat: 0
+            });
+            windOn = 0;
+        }
     });
     windy.on('pointerout', function (pointer) {
-        windy.setScale(0.13);
-        if (bigW == true) {
+        windy.setScale(0.13*basicScale);
+        if (mouseOver == true) {
             ckSound.play();
         };
-        bigW = false;
-        windOn = 0;
+        mouseOver = false;
     });
     windy.on('pointerup', function (pointer) {
-        windy.setScale(0.13);
-        if (bigW == true) {
+        windy.setScale(0.13*basicScale);
+        if (mouseOver == true) {
             ckSound.play();
         };
-        bigW = false;
-        windOn = 0;
-    });*/
+        mouseOver = false;
+    });
+
+    
+    
+    //Creating the Solar Panels
+    var solar = this.add.sprite(sceneWidth*0.62, sceneHeight*0.57, 'solar').setInteractive();
+    solar.setScale(0.05*basicScale);
+    var solar2 = this.add.sprite(sceneWidth*0.66, sceneHeight*0.56, 'solar').setInteractive();
+    solar2.setScale(0.05*basicScale);
+
+    solar.on('pointerdown', function (pointer) {
+        cliSound.play();
+        solar.setScale(0.053*basicScale);
+        mouseOver = true;
+    });
+    solar.on('pointerout', function (pointer) {
+        solar.setScale(0.05*basicScale);
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
+    });
+    solar.on('pointerup', function (pointer) {
+        solar.setScale(0.05*basicScale);
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
+    });
+
+    solar2.on('pointerdown', function (pointer) {
+        cliSound.play();
+        solar2.setScale(0.053*basicScale);
+        mouseOver = true;
+    });
+    solar2.on('pointerout', function (pointer) {
+        solar2.setScale(0.05*basicScale);
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
+    });
+    solar2.on('pointerup', function (pointer) {
+        solar2.setScale(0.05*basicScale);
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
+    });
+
 
     //Make the powerplant
     var pp = this.add.sprite(sceneWidth*0.9, sceneHeight*0.56, 'powerplant').setInteractive();
     pp.setScale(0.3*basicScale);
+    
     pp.on('pointerdown', function (pointer) {
         cliSound.play();
         pp.setScale(0.32*basicScale);
-        bigP = true;
+        mouseOver = true;
     });
     pp.on('pointerout', function (pointer) {
         pp.setScale(0.3*basicScale);
-        if (bigP == true) {
+        if (mouseOver == true) {
             ckSound.play();
         };
-        bigP = false;
+        mouseOver = false;
     });
     pp.on('pointerup', function (pointer) {
         pp.setScale(0.3*basicScale);
-        if (bigP == true) {
+        if (mouseOver == true) {
             ckSound.play();
         };
-        bigP = false;
+        mouseOver = false;
+    });
+
+    //Block the sun
+    var sun = this.add.sprite(sceneWidth*0.595, sceneHeight*0.135, 'sun').setInteractive();
+    sun.setScale(basicScale*0.8);
+    
+    sun.on('pointerdown', function (pointer) {
+        cliSound.play();
+        mouseOver = true;
+        if (night == 1) {
+            nightbg.setScale(0.001);
+            night = 0;
+        } else {
+            nightbg.setScale(basicScale);
+            night = 1;
+        }
+    });
+    sun.on('pointerout', function (pointer) {
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
+    });
+    sun.on('pointerup', function (pointer) {
+        if (mouseOver == true) {
+            ckSound.play();
+        };
+        mouseOver = false;
     });
 };
-
-function clickScale(spriteName,itemScale) {
-    spriteName.setScale(itemScale*basicScale);
-    spriteName.on('pointerdown', function (pointer) {
-        cliSound.play();
-        spriteName.setScale(1.07*itemScale*basicScale);
-        bigP = true;
-    });
-    spriteName.on('pointerout', function (pointer) {
-        pp.setScale(itemScale*basicScale);
-        if (bigP == true) {
-            ckSound.play();
-        };
-        bigP = false;
-    });
-    spriteName.on('pointerup', function (pointer) {
-        spriteName.setScale(itemScale*basicScale);
-        if (bigP == true) {
-            ckSound.play();
-        };
-        bigP = false;
-    });
-}
-
 
 //config
 var config = {
     type: Phaser.AUTO,
     width: sceneWidth,
     height: sceneHeight,
+    pixelArt: true,
     scene: gameScene
 };
 
